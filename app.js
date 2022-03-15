acfcf730d2cef4e8528ca8c8f1920d563171f08e
ojas thakur <ojasthakurx@gmail.com>
2022-03-15 20:23:22 +0530

    qualities section complete

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ var clientHeight = document.getElementById('nav-links').clientHeight;
 // alert(`nav-links height: ${clientHeight}`)
 
 // Create showLinks class dynamically
+
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
